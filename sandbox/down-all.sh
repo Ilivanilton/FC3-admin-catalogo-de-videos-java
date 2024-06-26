635df13a4ffd45a821d39054e4824b2966037b2a
ilivanilton <ilivanilton@lccv.ufal.br>
2024-06-26 18:22:59 -0300

    add

---
+++ sandbox/down-all.sh
@@ -0,0 +1,9 @@
+
+echo "Iniciando shutdown nos containers..."
+
+docker compose -f services/docker-compose.yml down
+docker compose -f elk/docker-compose.yml down
+docker compose -f app/docker-compose.yml down
+
+echo "shutdown finalizado!"
+
